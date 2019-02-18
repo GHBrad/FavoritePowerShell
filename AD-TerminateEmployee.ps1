552096e263c7adf452aa1d9a39e702a08cfd0b9e
GHBrad <41804577+GHBrad@users.noreply.github.com>
2019-02-18 08:07:54 -0700

    Update AD-TerminateEmployee.ps1

--- AD-TerminateEmployee.ps1
+++ AD-TerminateEmployee.ps1
@@ -6,24 +6,43 @@ Import-Module activedirectory
 
 #Create Exchange PowerShell session
 $UserCredential = Get-Credential 
-$ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://<EXCHANGE SERVER>/PowerShell -Authentication Kerberos -Credential $UserCredential
+$ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://<EXCHANGE SERVER URL>/PowerShell -Authentication Kerberos -Credential $UserCredential
 Import-PSSession $ExchSession -DisableNameChecking -AllowClobber | Out-Null
 
 #Prompt for user to be terminated
-$TerminateUser = Read-Host -Prompt 'Input the username of the employee being terminated. Example: bsaget'
+$TerminatedUser = Read-Host -Prompt 'Input the username of the employee being terminated. Example: bsaget'
 
-#Export the email to a network share
-New-MailboxExportRequest -Mailbox $TerminateUser -FilePath "\\<NETWORK SHARE>\exported email\'$TerminateUser.pst"
+#Export the email to a network share then move the export to their H: drive
+Get-MailboxExportRequest | Where-Object Status -eq "Completed" | Remove-MailboxExportRequest -Confirm:$False
+$RequestID = (New-MailboxExportRequest -Mailbox $TerminatedUser -FilePath "\\<NETWORK SHARE>\$TerminatedUser.pst").RequestGuid.Guid
+
+$strStatus = (Get-MailboxExportRequest -Identity $RequestID).Status
+DO {
+    $strStatus = (Get-MailboxExportRequest -Identity $RequestID).Status
+    Start-Sleep 10
+    Clear-Host
+    $strCurrDateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "Status as of ${strCurrDateTime}: ${strStatus}"
+} UNTIL ($strStatus -eq "Completed" -or $strStatus -eq "Failed")
+
+IF ($StrStatus -eq "Completed") {
+    New-Item -ItemType Directory -Path "\\<NETWORK SHARE>\$TerminatedUser" -Name "Mailbox Export" -Force
+    Move-Item "\\<NETWORK SHARE>\$TerminatedUser.pst" -Destination "\\<NETWORK SHARE>\$TerminatedUser\Mailbox Export\" -Force
+}
+
+IF ($StrStatus -eq "Failed") {
+    Write-Host "Mailbox export failed. You can try running this script again or investigate."
+}
 
 #Gather list of AD groups the user is a member of
-$ADGroups = Get-ADPrincipalGroupMembership $TerminateUser | where {$_.Name -ne "Domain Users"} 
+$ADGroups = Get-ADPrincipalGroupMembership $TerminatedUser | where {$_.Name -ne "Domain Users"} 
 
 #Remove from AD groups and export groups to txt file
-$ADGroups | Export-Csv "\\<NETWORK SHARE>\TerminatedEmployeeGroupMembership\'$TerminateUser.txt"
-Remove-ADPrincipalGroupMembership -Identity $TerminateUser -MemberOf $ADGroups -Confirm:$false
+$ADGroups | Export-Csv "\\<NETWORK SHARE\TerminatedEmployeeGroupMembership\'$TerminatedUser.txt"
+Remove-ADPrincipalGroupMembership -Identity $TerminatedUser -MemberOf $ADGroups -Confirm:$false
 
 #Move user to Terminated Employees OU
-Get-ADUser $TerminateUser | Move-ADObject -TargetPath "<TARGET OU>"
+Get-ADUser $TerminatedUser | Move-ADObject -TargetPath "OU=Terminated Employees,<OU PATH>"
 
 #Ends the Exchange session
 Remove-PSSession -Session $ExchSession
