d0c497beb41e8c8868bf79a7492998580cb87551
GHBrad <41804577+GHBrad@users.noreply.github.com>
2019-03-12 10:06:08 -0600

    Update Install-PrinterLocally.ps1

--- Install-PrinterLocally.ps1
+++ Install-PrinterLocally.ps1
@@ -4,7 +4,6 @@
 #I haven't been able to figure out how to set a relative path to install the INF file using pnputil, Add-WindowsDriver or Add-PrinterDriver. So,
 #if you're going to use this script as a template for other printer installs, change the printer named folder ('c:\temp\<PRINTER NAME>\OEMSETUP.INF) to the appropriate folder name.
 
-
 [CmdletBinding()]
     Param
     (
@@ -26,17 +25,27 @@
         $DRIVERPATH="<FOLDER PATH OF DRIVER>"
     )
 
+#If the name is blank, use localhost
+$InstallingLocally = $FALSE
+if ($ComputerName -eq ""){
+    $ComputerName = $env:COMPUTERNAME
+    $InstallingLocally = $TRUE
+}
 
-        # Remove printer if already installed
-        Remove-Printer -ComputerName $ComputerName -Name $PRINTERNAME
-        Remove-PrinterPort -computername $computername -Name $PRINTERIP
+# Remove printer if already installed
+Remove-Printer -ComputerName $ComputerName -Name $PRINTERNAME
+Remove-PrinterPort -computername $computername -Name $PRINTERIP
 
-        # Copy and install drivers to machine
-        robocopy "\\<NETWORK SHARE>\$DRIVERPATH" "\\$ComputerName\c$\temp\$PRINTERNAME" /mir
-        Invoke-Command -ComputerName $ComputerName -ScriptBlock {pnputil.exe /a "C:\temp\PRINTER DRIVERS\OEMSETUP.INF"}
+# Copy and install drivers to machine
+robocopy "\\nasty\share\downloads\Drivers\Windows\Printers\$DRIVERPATH" "\\$ComputerName\c$\temp\PRINTER DRIVERS" /mir
+if ($InstallingLocally){ #Installing locally. Run pnputil.exe directly.
+   pnputil.exe /a "C:\temp\PRINTER DRIVERS\OEMSETUP.INF"
+}else{ #Installing remotely. Run pnputil.exe remotely via Invoke-Command.
+   Invoke-Command -ComputerName $ComputerName -ScriptBlock {pnputil.exe /a "C:\temp\PRINTER DRIVERS\OEMSETUP.INF"}
+}
 
-        # Install printer and associate port
-        Add-PrinterDriver  -ComputerName $ComputerName -Name $DRIVERNAME
-        Add-PrinterPort -ComputerName $ComputerName -Name $PRINTERIP -PrinterHostAddress $PRINTERIP
-        Add-Printer -ComputerName $ComputerName -Name $PRINTERNAME -DriverName $DRIVERNAME -PortName $PRINTERIP
+# Install printer and associate port
+Add-PrinterDriver  -ComputerName $ComputerName -Name $DRIVERNAME
+Add-PrinterPort -ComputerName $ComputerName -Name $PRINTERIP -PrinterHostAddress $PRINTERIP
+Add-Printer -ComputerName $ComputerName -Name $PRINTERNAME -DriverName $DRIVERNAME -PortName $PRINTERIP
 
